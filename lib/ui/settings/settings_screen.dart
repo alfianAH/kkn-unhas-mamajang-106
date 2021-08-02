8a8f59a88e1890c90e4db2d127dc541bfb9a9d0d
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-03 00:22:28 +0800

    Make dark mode switch

--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+import 'package:kkn_unhas_mamajang_106/ui/values/colors/app_colors.dart';
 
 class SettingsScreen extends StatefulWidget{
   @override
@@ -6,11 +8,14 @@ class SettingsScreen extends StatefulWidget{
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkMode = true;
+
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
 
     return Container(
+      padding: const EdgeInsets.all(18),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -25,6 +30,41 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
 
           SizedBox(height: 8,),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Dark mode',
+                style: textTheme.headline4,
+              ),
+
+              FlutterSwitch(
+                value: isDarkMode,
+                height: 30,
+                width: 60,
+                activeColor: AppColors.fontDark,
+                activeToggleColor: AppColors.bodyLight,
+                activeIcon: Icon(
+                  Icons.dark_mode_rounded,
+                  color: AppColors.fontDark,
+                ),
+
+                inactiveColor: AppColors.bodyLight,
+                inactiveToggleColor: AppColors.fontDark,
+                inactiveIcon: Icon(
+                  Icons.light_mode_rounded,
+                  color: AppColors.fontLight,
+                ),
+                onToggle: (value){
+                  setState(() {
+                    isDarkMode = value;
+                  });
+                }
+              ),
+            ],
+          ),
         ],
       ),
     );
