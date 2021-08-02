b1e8cb7da9643c6a6dcfc22dac6944c666249954
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-02 23:04:23 +0800

    Add header in news screen

--- lib/ui/news/news_screen.dart
+++ lib/ui/news/news_screen.dart
@@ -5,11 +5,28 @@ class NewsScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
 
-    return Container(
-      child: Text(
-        'News',
-        style: textTheme.headline1,
-      ),
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(18, 18, 18, 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Berita',
+              style: textTheme.headline1,
+            ),
+
+            Divider(
+              color: textTheme.headline1!.color,
+              thickness: 2,
+            ),
+
+            SizedBox(height: 8,),
+
+            // News Provider List
+          ],
+        )
+      )
     );
   }
 }
