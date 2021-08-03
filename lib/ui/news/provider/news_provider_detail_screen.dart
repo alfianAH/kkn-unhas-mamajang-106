35d7c7e299823ee91576b9d8fb69e66eb75459e0
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-03 16:32:55 +0800

    Make news provider detail screen header

---
+++ lib/ui/news/provider/news_provider_detail_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NewsProviderDetailScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(18),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Berita Harian',
+              style: textTheme.headline1,
+            ),
+
+            Divider(
+              color: textTheme.headline1!.color,
+              thickness: 2,
+            ),
+
+            SizedBox(height: 8,),
+
+            // News List
+            
+          ]
+        ),
+      ),
+    );
+  }
+}
