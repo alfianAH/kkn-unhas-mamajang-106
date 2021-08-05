8e1e4dad032a2285c1a041272b34084a4c724983
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-05 21:39:17 +0800

    Separate provider for poster and news

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kkn_unhas_mamajang_106/service/news/news_provider_service.dart';
+import 'package:kkn_unhas_mamajang_106/service/poster/poster_provider_service.dart';
 import 'package:kkn_unhas_mamajang_106/ui/home/home_screen.dart';
 import 'package:kkn_unhas_mamajang_106/ui/values/theme/app_theme.dart';
 import 'package:logging/logging.dart';
@@ -26,8 +27,8 @@ class MyApp extends StatelessWidget {
       valueListenable: _notifier,
       builder: (BuildContext context, ThemeMode mode, Widget? child){
         return Provider(
-          create: (_) => NewsProviderService.create(),
-          dispose: (_, NewsProviderService service) => service.client.dispose(),
+          create: (_) => PosterProviderService.create(),
+          dispose: (_, PosterProviderService service) => service.client.dispose(),
           child: MaterialApp(
             title: 'KKN Unhas Mamajang G-106',
             theme: AppTheme.lightTheme,
--- lib/ui/news/news_screen.dart
+++ lib/ui/news/news_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kkn_unhas_mamajang_106/service/news/news_provider_service.dart';
 import 'package:kkn_unhas_mamajang_106/ui/news/list/provider/news_provider_list.dart';
+import 'package:provider/provider.dart';
 
 /// [NewsScreen] is for news providers and news list
 class NewsScreen extends StatefulWidget{
@@ -14,30 +16,34 @@ class _NewsScreenState extends State<NewsScreen> {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
 
-    return SingleChildScrollView(
-      controller: _scrollController,
-      child: Container(
-        padding: const EdgeInsets.all(18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Berita Harian',
-              style: textTheme.headline1,
-            ),
+    return Provider(
+      create: (_) => NewsProviderService.create(),
+      dispose: (_, NewsProviderService service) => service.client.dispose(),
+      child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Container(
+            padding: const EdgeInsets.all(18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Berita Harian',
+                  style: textTheme.headline1,
+                ),
 
-            Divider(
-              color: textTheme.headline1!.color,
-              thickness: 2,
-            ),
+                Divider(
+                  color: textTheme.headline1!.color,
+                  thickness: 2,
+                ),
 
-            SizedBox(height: 8,),
+                SizedBox(height: 8,),
 
-            // News Provider List
-            NewsProviderList(),
-          ],
-        )
-      )
+                // News Provider List
+                NewsProviderList(),
+              ],
+            )
+          )
+      ),
     );
   }
 
