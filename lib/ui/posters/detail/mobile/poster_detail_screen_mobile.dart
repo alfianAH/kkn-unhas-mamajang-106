250fc80965b8b4a7ff13aa82e7b55369c088da4c
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-06 23:09:02 +0800

    Make responsive poster detail screen

---
+++ lib/ui/posters/detail/mobile/poster_detail_screen_mobile.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:kkn_unhas_mamajang_106/model/poster/poster_model.dart';
+import 'package:kkn_unhas_mamajang_106/utils/date_converter.dart';
+import 'package:kkn_unhas_mamajang_106/utils/image_asset_loader.dart';
+
+class PosterDetailScreenMobile extends StatelessWidget{
+  final PosterData posterData;
+  final String _dateFormat = 'EEEE, dd-MM-yy';
+
+  PosterDetailScreenMobile({
+    required this.posterData,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      padding: const EdgeInsets.all(18),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Title
+          Row(
+            children: [
+              IconButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios_rounded,
+                  color: textTheme.headline1!.color,
+                ),
+              ),
+
+              SizedBox(width: 8),
+
+              Text(
+                'Poster ${MediaQuery.of(context).size.width}',
+                style: textTheme.headline1,
+              )
+            ],
+          ),
+
+          Divider(
+            color: textTheme.headline1!.color,
+            thickness: 2,
+          ),
+
+          SizedBox(height: 8,),
+
+          // Details content
+          Container(
+            width: 600,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Poster title
+                SelectableText(
+                  posterData.title ?? '',
+                  style: textTheme.headline2,
+                ),
+
+                SizedBox(height: 8,),
+
+                // Poster date and author
+                SelectableText(
+                  '${DateConverter.convertDate(posterData.isoDate.toString(), _dateFormat)} '
+                      'oleh ${posterData.author}',
+                  style: textTheme.subtitle1
+                ),
+
+                SizedBox(height: 16,),
+
+                // Poster image
+                Center(
+                  child: Container(
+                    width: 250,
+                    child: ImageAssetLoader(
+                      image: posterData.image ?? '',
+                      boxFit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 16,),
+
+                // Poster content
+                SelectableText(
+                  posterData.contentSnippet ?? '',
+                  style: textTheme.bodyText1,
+                ),
+
+                SizedBox(height: 8,),
+              ],
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
--- lib/ui/posters/detail/poster_detail_screen.dart
+++ lib/ui/posters/detail/poster_detail_screen.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:kkn_unhas_mamajang_106/model/poster/poster_model.dart';
-import 'package:kkn_unhas_mamajang_106/ui/values/colors/app_colors.dart';
-import 'package:kkn_unhas_mamajang_106/utils/date_converter.dart';
-import 'package:kkn_unhas_mamajang_106/utils/image_asset_loader.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:kkn_unhas_mamajang_106/ui/posters/detail/mobile/poster_detail_screen_mobile.dart';
+import 'package:kkn_unhas_mamajang_106/ui/posters/detail/web/poster_detail_content_web.dart';
 
 class PosterDetailScreen extends StatefulWidget{
   final PosterData posterData;
@@ -19,98 +16,21 @@ class PosterDetailScreen extends StatefulWidget{
 
 class _PosterDetailScreenState extends State<PosterDetailScreen> {
   ScrollController _scrollController = ScrollController();
-  final String _dateFormat = 'EEEE, dd-MM-yy';
 
   @override
   Widget build(BuildContext context) {
-    TextTheme textTheme = Theme.of(context).textTheme;
-
     return Scaffold(
       body: SingleChildScrollView(
         controller: _scrollController,
-        child: Container(
-          padding: const EdgeInsets.all(18),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Title
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: (){
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.arrow_back_ios_rounded,
-                      color: textTheme.headline1!.color,
-                    ),
-                  ),
-
-                  SizedBox(width: 8),
-
-                  Text(
-                    'Poster',
-                    style: textTheme.headline1,
-                  )
-                ],
-              ),
-
-              Divider(
-                color: textTheme.headline1!.color,
-                thickness: 2,
-              ),
-
-              SizedBox(height: 8,),
-
-              // Details content
-              Container(
-                width: 600,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Poster title
-                    SelectableText(
-                      widget.posterData.title ?? '',
-                      style: textTheme.headline2,
-                    ),
-
-                    SizedBox(height: 8,),
-
-                    // Poster date and author
-                    SelectableText(
-                      '${DateConverter.convertDate(widget.posterData.isoDate.toString(), _dateFormat)} '
-                          'oleh ${widget.posterData.author}',
-                      style: textTheme.subtitle1
-                    ),
-
-                    SizedBox(height: 16,),
-
-                    // Poster image
-                    Center(
-                      child: Container(
-                        width: 250,
-                        child: ImageAssetLoader(
-                          image: widget.posterData.image ?? '',
-                          boxFit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(height: 16,),
-
-                    // Poster content
-                    SelectableText(
-                      widget.posterData.contentSnippet ?? '',
-                      style: textTheme.bodyText1,
-                    ),
-
-                    SizedBox(height: 8,),
-                  ],
-                ),
-              ),
-            ]
-          ),
-        ),
+        child:LayoutBuilder(
+          builder: (context, constraints) {
+            if(constraints.maxWidth < 800){
+              return PosterDetailScreenMobile(posterData: widget.posterData);
+            } else {
+              return PosterDetailContentWeb(posterData: widget.posterData);
+            }
+          },
+        )
       ),
     );
   }
